dc695a1c3a3ff223042697d1cabd9c5fc495675f
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-12-11 03:58:43 -0500

    addBook mutation complete

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -38,13 +38,13 @@ const resolvers = {
     },
 
     // Add a third argument to the resolver to access data in our `context`
-    addSkill: async (parent, { profileId, skill }, context) => {
+    addBook: async (parent, args, context) => {
       // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
       if (context.user) {
-        return Profile.findOneAndUpdate(
-          { _id: profileId },
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
           {
-            $addToSet: { skills: skill },
+            $addToSet: { savedBooks: args },
           },
           {
             new: true,
@@ -56,7 +56,7 @@ const resolvers = {
       throw new AuthenticationError('You need to be logged in!');
     },
     // Make it so a logged in user can only remove a skill from their own profile
-    removeSkill: async (parent, { skill }, context) => {
+    removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
         return Profile.findOneAndUpdate(
           { _id: context.user._id },
